33ba580023e7451acdc7738c3c8f279d07c1ae35
hiroto-hara <xre0605@gmail.com>
2024-04-23 21:03:52 +0900

    リレーションの設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,14 @@ class User extends Authenticatable
         'password' => 'hashed',
         'id' => 'string'
     ];
+
+    public function recipes()
+    {
+        return $this->hasMany(Recipe::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
